91613c380e96266cc920ae591739dc5e8d44ba44
Роман999-альт <vojcehovicroma@gmail.com>
2026-03-10 22:45:08 +0300

    clean deploy

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+const TOKEN = "8440870990:AAFc8eim67c5M7_jhFvDQAF0zmvygdc3GLc";
+const CHAT_ID = "5058136349";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.post("/send", async (req, res) => {
+
+const { name, phone, service, comment } = req.body;
+
+const text = `
+🔥 Новая заявка с сайта
+
+👤 Имя: ${name}
+📞 Телефон: ${phone}
+🐜 Проблема: ${service}
+📍 Адрес: ${comment}
+`;
+
+try {
+
+const response = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`,{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+chat_id: CHAT_ID,
+text: text
+})
+});
+
+const data = await response.json();
+
+if(data.ok){
+res.json({ok:true});
+}else{
+res.json({ok:false,error:data.description});
+}
+
+}catch(error){
+
+console.log(error);
+res.json({ok:false,error:"Telegram error"});
+
+}
+
+});
+
+console.log("🚀 Сервер запущен");
+
+app.listen(PORT,()=>{
+console.log(`Server running on port ${PORT}`);
+});
